54a71eaf0200149e965b4c4ba4a42214f6c0ff39
Pavel <pasha.tomiloff@gmail.com>
2025-03-31 10:30:17 +0300

    final

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // Main JavaScript file
 "use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleMenu = document.querySelector('.burger');
+  var menu = document.querySelector('.nav');
+  toggleMenu.addEventListener('click', function () {
+    this.classList.toggle('active');
+    menu.classList.toggle('open');
+
+    // Блокировка прокрутки страницы
+    if (menu.classList.contains('open')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  });
+
+  // Закрытие меню при клике на ссылку
+  var navLinks = document.querySelectorAll('.nav__item-link');
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      toggleMenu.classList.remove('active');
+      menu.classList.remove('open');
+      document.body.style.overflow = '';
+    });
+  });
+});
